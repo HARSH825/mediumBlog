78fdbe57b585d2c6f6d5887ab9fce89e8391edc5
HARSH825 <harshchhallani3@gmail.com>
2025-05-27 11:22:31 +0530

    Change default route

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -4,13 +4,13 @@ export const Quote = () => {
         <div className="flex justify-center">
             <div className="max-w-lg">
                 <div className="text-3xl font-bold">
-                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
+                    There’s an entrepreneur right now, scared to death, making excuses, saying, “It’s not the right time just yet. There’s no such thing as a good time."
                 </div>
                 <div className="max-w-md text-xl font-semibold text-left mt-4">
-                    Julies Winfield
+                    Gary Vee 
                 </div>
                 <div className="max-w-md text-sm font-light text-slate-400">
-                    CEO | Acme corp
+                    CEO | VaynerMedia
                 </div>
             </div>
         </div>
